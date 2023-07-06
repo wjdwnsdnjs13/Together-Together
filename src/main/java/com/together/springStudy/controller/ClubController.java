83819dc0c027f9c798bc16ec301897a76533fe53
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-07-06 20:41:53 +0900

    Create ClubController.java

---
+++ src/main/java/com/together/springStudy/controller/ClubController.java
@@ -0,0 +1,11 @@
+package com.together.springStudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/together/club")
+@Controller
+public class ClubController {
+
+
+}
