8ea1252aa18b61b789991a97cecbeb65eb8cf4ad
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-07-16 17:48:07 +0900

    RestController로 변경, getAllClub 수정

--- src/main/java/com/together/springStudy/controller/ClubController.java
+++ src/main/java/com/together/springStudy/controller/ClubController.java
@@ -3,20 +3,22 @@ package com.together.springStudy.controller;
 import com.together.springStudy.model.ClubData;
 import com.together.springStudy.model.UserData;
 import com.together.springStudy.service.ClubService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/together/club")
-@Controller
 @Slf4j
+@RequestMapping("/together/club")
+@RequiredArgsConstructor
+@RestController
 public class ClubController {
 
     @Autowired
@@ -33,7 +35,9 @@ public class ClubController {
 
     @GetMapping("/getAllClub")
     public List<ClubData> getAllClub(){
+        log.debug("getAllClub play.");
         List<ClubData> clubDataList = clubService.getAllClub();
+        log.debug("club List : {}", clubDataList);
         return clubDataList;
 //        clubDataList가 안 불러와졌을 때 Bad Request날릴 수 있어야할 듯?
 //        if(clubDataList != null) return clubDataList;
