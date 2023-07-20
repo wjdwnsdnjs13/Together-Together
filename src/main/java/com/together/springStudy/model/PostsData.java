a5773b7a910d27aa5ea4400443d4d9a7aa51d50f
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-07-20 22:33:37 +0900

    게시물id, 태그, 생성일자 제외 생성자

--- src/main/java/com/together/springStudy/model/PostsData.java
+++ src/main/java/com/together/springStudy/model/PostsData.java
@@ -1,10 +1,12 @@
 package com.together.springStudy.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
 public class PostsData {
     int postId;
     int postBoardId;
@@ -13,4 +15,15 @@ public class PostsData {
     String postContent;
     String postTag;
     Date postCreationDate;
+
+    public PostsData(){};
+
+    public PostsData(int postBoardId, int postUserId, String postTitle, String postContent) {
+        this.postBoardId = postBoardId;
+        this.postUserId = postUserId;
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+    }
+
+
 }
