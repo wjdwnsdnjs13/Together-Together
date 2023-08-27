c7a88c8b9c6c8ba49d2c7d21a17ec3eb5f92794f
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-08-27 12:32:58 +0900

    클럽 조회 시 리더 닉네임과 클럽 회원 수 함께 표시

---
+++ src/main/java/com/together/springStudy/model/ClubListData.java
@@ -0,0 +1,12 @@
+package com.together.springStudy.model;
+
+public class ClubListData {
+    int clubId;
+    int clubLeaderId;
+    int clubBoardId;
+    String clubName;
+    String clubDescription;
+    boolean clubRecruiting;
+    String clubMasterNickname;
+    int clubMemberCount;
+}
